fd1090d7dc2fee540e7d0ee3e632fb64753df31b
Mark <m.biem@outlook.com>
2021-04-15 02:04:40 +0300

    fix merge singin

--- tests/moneycat/pages/auth/components/signin_form.py
+++ tests/moneycat/pages/auth/components/signin_form.py
@@ -8,7 +8,6 @@ class SignInForm(Component):
     class Selectors:
         login = 'input[type="email"]'
         password = 'input[type="password"]'
-<<<<<<< HEAD
         submit_button = 'input[type="submit"]'
         account_header = 'p[id="account"]'
         href_to_signin = 'a[class="signin-link"]'
@@ -27,38 +26,16 @@ class SignInForm(Component):
     def get_error_message(self) -> str:
         self._wait_visible(By.CSS_SELECTOR, self.Selectors.error_message)
         return self.driver.find_element_by_css_selector(self.Selectors.error_message).text
-=======
-        next_button = '[data-test-id="next-button"]'
-        submit_button = 'input[type="submit"]'
-        user_email_header = 'p[id="account"]'
-        redirect_to_signin = 'a[class="signin-link"]'
-
-    def go_to_signin(self):
-        self._wait_clickable(By.CSS_SELECTOR, self.Selectors.redirect_to_signin)
-        return self.driver.find_element_by_css_selector(self.Selectors.redirect_to_signin).click()
-
-    def get_user_email(self) -> str:
-        self._wait_clickable(By.CSS_SELECTOR, self.Selectors.user_email_header)
-        return self.driver.find_element_by_css_selector(self.Selectors.user_email_header).text
->>>>>>> master
 
     def fill_login(self, username):
         self._fill_input(By.CSS_SELECTOR, self.Selectors.login, username)
 
-<<<<<<< HEAD
-=======
-    def next(self):
-        self._wait_visible(By.CSS_SELECTOR, self.Selectors.next_button)
-        self.driver.find_element_by_css_selector(self.Selectors.next_button).click()
-
->>>>>>> master
     def fill_password(self, password):
         self._fill_input(By.CSS_SELECTOR, self.Selectors.password, password)
 
     def submit(self):
         self._wait_clickable(By.CSS_SELECTOR, self.Selectors.submit_button)
         self.driver.find_element_by_css_selector(self.Selectors.submit_button).click()
-<<<<<<< HEAD
 
     def click_signup_href(self):
         self._wait_clickable(By.CSS_SELECTOR, self.Selectors.href_to_signup)
@@ -67,5 +44,3 @@ class SignInForm(Component):
     def get_registration_title(self):
         self._wait_visible(By.CSS_SELECTOR, self.Selectors.registration_title)
         return self.driver.find_element_by_css_selector(self.Selectors.registration_title).text
-=======
->>>>>>> master
