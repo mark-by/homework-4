5d3c2b39b21be31098b9646ae7f76fb6c548ecc5
Mark <m.biem@outlook.com>
2021-04-15 02:39:36 +0300

    one driver

--- testutils/selenium_test.py
+++ testutils/selenium_test.py
@@ -1,14 +1,5 @@
 import unittest
-
-from selenium.webdriver import Remote, DesiredCapabilities
 from .utils import CurrentBrowser
-
-
-def get_driver(browser):
-    return Remote(
-        command_executor='http://127.0.0.1:4444/wd/hub',
-        desired_capabilities=getattr(DesiredCapabilities, browser).copy()
-    )
 
 
 GlobalCurrentBrowser = CurrentBrowser()
@@ -17,7 +8,7 @@ GlobalCurrentBrowser = CurrentBrowser()
 class SeleniumTest(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.driver = get_driver(GlobalCurrentBrowser.get())
+        self.driver = GlobalCurrentBrowser.get()
 
     def tearDown(self) -> None:
         self.driver.quit()
--- testutils/utils.py
+++ testutils/utils.py
@@ -1,9 +1,21 @@
+from selenium.webdriver import Remote, DesiredCapabilities
+
+
 class CurrentBrowser:
     def __init__(self):
         self.browser = None
+        self.driver = None
 
     def set(self, browser):
         self.browser = browser
+        self.driver = self.__get_driver(browser)
 
     def get(self):
-        return self.browser
+        return self.driver
+
+    @staticmethod
+    def __get_driver(browser):
+        return Remote(
+            command_executor='http://127.0.0.1:4444/wd/hub',
+            desired_capabilities=getattr(DesiredCapabilities, browser).copy()
+        )
