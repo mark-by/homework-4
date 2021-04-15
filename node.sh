b54abfd33d49da6710a5d1b7e48c9acf913b613e
Mark <m.biem@outlook.com>
2021-04-15 21:17:52 +0300

    refactor scripts

--- node.sh
+++ node.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-java -Dwebdriver.chrome.driver=$1 \
-    -Dwebdriver.gecko.driver=$2 \
+java -Dwebdriver.chrome.driver="./$1" \
+    -Dwebdriver.gecko.driver="./$2" \
     -jar $3 \
     -role node \
     -hub http://127.0.0.1:4444/grid/register \
--- settings.py
+++ settings.py
@@ -2,8 +2,8 @@ from tests.todo import TaskTest, TaskListTest, SortTest
 from tests.moneycat import MoneyCatAuthTest, MoneyCatRegistrationTest, MoneyCatConverterTest, MoneyCatSettingsTest,\
     MoneyCatMainTest
 
-CHROME_BIN = './bin/mac_m1/chromedriver'
-FIREFOX_BIN = './bin/mac_m1/geckodriver'
+CHROME_BIN = 'bin/mac_m1/chromedriver'
+FIREFOX_BIN = 'bin/mac_m1/geckodriver'
 SELENIUM_BIN = 'bin/selenium.jar'
 
 TESTS = [
